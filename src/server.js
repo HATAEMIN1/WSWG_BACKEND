34b5af44e501078578f09c36c7e18222b8204d41
HaTaeMin <gkxoals33@gmail.com>
2024-05-08 10:15:55 +0900

    식당리스트,식당 뷰

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const { userRouter } = require("./routers/userRouter");
+const restaurantRouter = require("./routers/restaurantRouter");
 
 dotenv.config();
 app.use(express.json());
@@ -15,6 +16,7 @@ const server = async () => {
     console.log("mongoDB connected");
     mongoose.set("debug", true);
     app.use("/user", userRouter);
+    app.use("/restaurant", restaurantRouter);
     app.listen(4000, async function () {
       console.log("server on port 4000");
     });
