4ae29a4c448d9df675f316813abe751f1ff53b5a
HaTaeMin <gkxoals33@gmail.com>
2024-05-13 23:31:44 +0900

    인피니티 스크롤 구현

--- src/routers/restaurantRouter.js
+++ src/routers/restaurantRouter.js
@@ -14,8 +14,14 @@ const mateType = [
 restaurantRouter.get("/:cateId", async (req, res) => {
   try {
     const { cateId } = req.params;
-    const restaurant = await Restaurant.find({ "category.mateType": cateId });
-    return res.status(200).send({ restaurant });
+    const limit = req.query.limit ? Number(req.query.limit) : 6;
+    const skip = req.query.skip ? Number(req.query.skip) : 0;
+    const restaurant = await Restaurant.find({ "category.mateType": cateId })
+      .limit(limit)
+      .skip(skip);
+    const productsTotal = await Restaurant.countDocuments();
+    const hasMore = skip + limit < productsTotal ? true : false;
+    return res.status(200).send({ restaurant, hasMore });
   } catch (e) {
     res.status(500).send({ error: e.message });
   }
