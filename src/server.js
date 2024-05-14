5c66c639362881a8a03c7472aa164c0294edbfe9
boram01 <qhfka676@naver.com>
2024-05-14 19:34:34 +0900

    name찾지못함

--- src/server.js
+++ src/server.js
@@ -9,14 +9,15 @@ const restaurantRouter = require("./routers/restaurantRouter");
 const meetUpPostCommentRouter = require("./routers/meetUpPostCommentRouter");
 const reviewRouter = require("./routers/reviewRouter");
 
-dotenv.config();
+dotenv.config(); //.env파필에있는것들을쓰되다른사람들한테보이지않게하기위해
 app.use(express.json());
-app.use(cors());
+app.use(cors()); //cors에러방지하기위해씀
 
 const server = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("mongoDB connected");
+
     mongoose.set("debug", true);
     app.use("/users", userRouter);
     app.use("/restaurants", restaurantRouter);
