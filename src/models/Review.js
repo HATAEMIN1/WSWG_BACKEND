b0058b4f65aae4cf08cb36ce0bcfd244f554d984
boram01 <qhfka676@naver.com>
2024-05-14 12:08:41 +0900

    리뷰

--- src/models/Review.js
+++ src/models/Review.js
@@ -1,14 +1,13 @@
 const { mongoose, Types } = require("mongoose");
 
 const reviewSchema = mongoose.Schema({
-  title: { type: String, required: true },
   content: { type: String, required: true },
   views: { type: Number },
   image: [{ type: String }],
   rating: { type: Number, required: true },
   createdAt: Date,
-  user: { type: Types.ObjectId, ref: "User" },
-  restaurant: { type: Types.ObjectId, ref: "Restaurant" },
+  user: { type: Types.ObjectId, ref: "user" },
+  restaurant: { type: Types.ObjectId, ref: "restaurant" },
 });
 const Review = mongoose.model("review", reviewSchema);
 module.exports = Review;
--- src/routers/reviewRouter.js
+++ src/routers/reviewRouter.js
@@ -1,32 +1,31 @@
+const express = require("express");
+const Reveiew = require("../models/Review");
+const User = require("../models/User");
+const Restaurant = require("../models/Restaurant");
+const reviewRouter = express.Router();
 
 reviewRouter.post("/", async (req, res) => {
-    try {
-      const { title, content, isLive, userId } = req.body;
-  
-      if (typeof title !== "string")
-        res.status(400).send({ err: "title is required" });
-      if (typeof content !== "string")
-        res.status(400).send({ err: "content is required" });
-      if (!mongoose.isValidObjectId(userId))
-        res.status(400).send({ err: "userId is required" });
-  
-      let user = await User.findById(userId);
-      if (!user) res.status(400).send({ err: "user does not" });
-  
-      // let blog = new Blog({...req.body,user})
-      // await blog.save()
-  
-      let blog = await new Blog({ ...req.body, user }).save();
-  
-      return res.status(200).send({ blog });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({ error: error.message });
-    }
-  });
+  try {
+    const { content, rating, userId, restId } = req.body;
+    console.log(userId);
+    console.log(restId);
+    console.log(req.body);
+    let user = await User.findById(userId);
+    let rest = await Restaurant.findById(restId);
 
-  reviewRouter.get("/",async (req,res)=>{
-    try {
-        let {}
-    }
-  })
+    const review = await new Reveiew({ ...req.body, user, rest }).save();
+
+    return res.status(200).send({ review });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: error.message });
+  }
+});
+
+// reviewRouter.get("/",async (req,res)=>{
+//   try {
+
+//   }
+// })
+
+module.exports = reviewRouter;
