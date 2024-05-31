808364d8bcaf71a4ea0a2e2e7877c7987ec4bb1f
boram <qhfka676@naver.com>
2024-05-31 15:27:16 +0900

    리뷰

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { mongoose } = require("mongoose");
 const User = require("../models/User");
-const Restaurant = require("../models/Restaurant");
+// const Restaurant = require("../models/Restaurant");
+const MeetUpPost = require("../models/MeetUpPost.js");
 const Like = require("../models/Like");
 const userRouter = express.Router();
 const { upload } = require("../middlewares/imageUpload.js");
@@ -302,8 +303,11 @@ userRouter.get("/:userId/likedResturants", async (req, res) => {
     const { userId } = req.params;
     if (!mongoose.isValidObjectId(userId))
       res.status(400).send({ error: "not a valid userId" });
-    const likes = await Like.find({ user: userId });
-    return res.status(200).send({ likes });
+    const likes = await Like.find({ user: userId, liked: true }).populate(
+      "restaurant"
+    );
+    const restaurants = likes.map((like) => like.restaurant);
+    return res.status(200).send({ restaurants });
   } catch (error) {
     console.log(error);
     res.status(500).send({ error });
@@ -329,6 +333,18 @@ userRouter.get("/:userId/likedResturants", async (req, res) => {
 // });
 
 // 내가 등록한 우리 만날까
+userRouter.get("/:userId/meetups", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    if (!mongoose.isValidObjectId(userId))
+      res.status(400).send({ error: "not a valid userId" });
+    const meetupPosts = await MeetUpPost.find({ user: userId });
+    return res.status(200).send({ meetupPosts });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
 
 userRouter.delete("/:userId", async function (req, res) {
   try {
@@ -385,7 +401,7 @@ userRouter.put(
           { new: true }
         );
         console.log("user with updated password and image:", user);
-        return res.send({ user });
+        return res.send({ user, passwordChange: true });
       } else if (!req.file && req.body.password) {
         const hashedPassword = await hash(req.body.password, 10);
         const user = await User.findByIdAndUpdate(
@@ -396,7 +412,7 @@ userRouter.put(
           { new: true }
         );
         console.log("user with updated password:", user);
-        return res.send({ user });
+        return res.send({ user, passwordChange: true });
       } else if (req.file && !req.body.password) {
         const { filename, originalname } = req.file;
         const image = { filename, originalname };
@@ -408,7 +424,7 @@ userRouter.put(
           { new: true }
         );
         console.log("user with updated image:", user);
-        return res.send({ user });
+        return res.send({ user, passwordChange: false });
       }
     } catch (error) {
       return res.status(500).send({ error: error.message });
